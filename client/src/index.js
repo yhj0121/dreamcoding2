35b0d7c5dc333782fc963d2a471ca1515f53dd6b
yoonhyeongjoon <boys1541@gmail.com>
2021-12-30 00:33:51 +0900

    second

--- client/src/index.js
+++ client/src/index.js
@@ -7,13 +7,15 @@ import TweetService from './service/tweet';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { AuthErrorEventBus } from './context/AuthContext';
+import TokenStorage from './db/token';
 import HttpClient from './network/http';
 
 const baseURL = process.env.REACT_APP_BASE_URL;
+const tokenStorage = new TokenStorage();
 const httpClient = new HttpClient(baseURL);
 const authErrorEventBus = new AuthErrorEventBus();
-const authService = new AuthService();
-const tweetService = new TweetService(httpClient);
+const authService = new AuthService(httpClient,tokenStorage);
+const tweetService = new TweetService(httpClient,tokenStorage);
 
 ReactDOM.render(
   <React.StrictMode>
